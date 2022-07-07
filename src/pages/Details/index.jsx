f0ca5e0a33ddf84b79b9b8c4d40f4a2be21f3105
Vander-Reis <vanderreis2017@outlook.com>
2022-07-07 15:00:19 -0300

    create page home

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,34 +1,50 @@
-import { Container, Links } from './styles'
+import { Container, Links, Content } from "./styles";
 
-import { Header } from '../../components/Header'
-import { Button } from '../../components/Button'
-import { Section } from '../../components/Section'
-import { Tag } from '../../components/Tag'
+import { Tag } from "../../components/Tag";
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { ButtonText } from "../../components/ButtonText";
+import { Section } from "../../components/Section";
 
 export function Details() {
-    return(
-        <Container>
-            <Header/>
-            <Section title="Links úteis">
-                <Links>
-                    <li>
-                        <a href="https://google.com">Link 1</a>
-                    </li>
+  return (
+    <Container>
+      <Header />
 
-                    <li>
-                        <a href="https://google.com">Link 1</a>
-                    </li>   
-                   
-                </Links>
-            </Section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
 
-            <Section title="Marcadores">
-                <Tag title="Express"/>
-                <Tag title="Node"/>
-            </Section>
-            
-            <Button title="Voltar" />
-        </Container>
-        
-    )
+          <h1>
+            Introdução ao React
+          </h1>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi odio, qui obcaecati optio nemo molestias reiciendis reprehenderit eos laudantium distinctio temporibus est nobis repellendus aliquid itaque aperiam, assumenda non natus!</p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="https://google.com">Link 1</a>
+              </li>
+
+              <li>
+                <a href="https://google.com">Link 2</a>
+              </li>
+
+              <li>
+                <a href="https://google.com">Link 3</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="Express" />
+            <Tag title="Node" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
+    </Container>
+  );
 }
