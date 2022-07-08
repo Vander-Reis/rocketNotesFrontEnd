b29a8fa2898d2cb8dece1fad5990a6ad5d1c776e
Vander-Reis <vanderreis2017@outlook.com>
2022-07-08 16:50:10 -0300

    Created pages SingIn/Profile

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,12 @@
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, FiSearch } from 'react-icons/fi';
 import  { Container, Brand, Menu, Search, Content, NewNote} from './styles';
 
 import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { Note } from '../../components/Note';
+import { Section } from '../../components/Section';
 import { ButtonText } from '../../components/ButtonText';
+
 
 export function Home() {
     return (
@@ -20,11 +24,21 @@ export function Home() {
             </Menu>
 
             <Search>
-
+                <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
             </Search>
 
             <Content>
-
+                <Section title="Minhas notas">
+                    <Note data={{
+                        title: "Node", 
+                        tags: [
+                            {id: '1', name: 'react'},
+                            {id: '2', name: 'node'},
+                        ]
+                        }}
+                        />
+                        
+                </Section>
             </Content>
 
             <NewNote>
